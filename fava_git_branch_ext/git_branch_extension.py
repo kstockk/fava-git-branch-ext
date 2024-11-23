a07db1d67dfe7559aa7552b98d26f1fa11746acd
Khanh Nguyen <nguyenk3@outlook.com>
2024-11-23 13:58:29 +1100

    added git status and graph to report page

--- fava_git_branch_ext/git_branch_extension.py
+++ fava_git_branch_ext/git_branch_extension.py
@@ -5,26 +5,36 @@ Usage:
 YYYY-MM-DD custom "fava-extension" "fava_git_branch_ext"
 """
 
+import os
 import subprocess
 from fava.ext import FavaExtensionBase
 
-class GitBranchExtension(FavaExtensionBase):
-    
-    report_title = "Current Branch"
+LEDGER_DATA_DIR = os.environ.get('LEDGER_DATA_DIR', '/Ledger')
 
-    def git_branch_name(self):
-        """Get the current Git branch name."""
+class GitBranchExtension(FavaExtensionBase):
+
+    report_title = "Source Control"
+
+    def _run_git_command(self, command):
         try:
             result = subprocess.run(
-                ["git", "rev-parse", "--abbrev-ref", "HEAD"],
-                cwd="/Ledger",  # Path to your git repo
+                command,
+                cwd=LEDGER_DATA_DIR,
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
                 text=True,
             )
             if result.returncode == 0:
                 return result.stdout.strip()
-            else:
-                return "No branch"
+            return f"Error: {result.stderr.strip()}"
         except Exception as e:
             return f"Error: {str(e)}"
+
+    def git_branch_name(self):
+        return self._run_git_command(["git", "rev-parse", "--abbrev-ref", "HEAD"])
+
+    def get_git_graph(self):
+        return self._run_git_command(["git", "log", "--graph", "--oneline", "--all", "--decorate"])
+
+    def get_git_status(self):
+        return self._run_git_command(["git", "status"])
